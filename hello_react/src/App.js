caf39be56e0cd4662d4c055c26c4f214ff5fa08a
KangHayeonn <rkdgkdus522@naver.com>
2021-12-15 16:09:03 +0900

    클래스형 컴포넌트의 state
    * constructor을 이용한 state 초기화
    * state를 constructor에서 꺼내기
    * setState를 이용한 state 값 변경
    * this.setState에 객체 대신 함수 인자 전달하기
    
    - 작성자 : 강하연
    - 작성일 : 2021-12-15

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 function App() {
-  const name = "강하연";
-  return (
-    <div
-      className="react" // 이부분도 될라나?
-    >
-      <MyComponent name={name}>리액트</MyComponent>
-    </div>
-  );
+  return <Counter />;
 }
 
 export default App;
