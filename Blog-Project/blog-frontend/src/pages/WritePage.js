89b060ca532b0988990a32a7ef8e6a67f7d040ef
KangHayeonn <rkdgkdus522@naver.com>
2022-01-30 08:05:09 +0900

    리덕스로 글쓰기 상태 관리하기
    * title, body, tags 리덕스 출력 확인 (리덕스 스토어에 반영)
    
    - 작성자 : 강하연
    - 작성일 : 2022-01-30

--- Blog-Project/blog-frontend/src/pages/WritePage.js
+++ Blog-Project/blog-frontend/src/pages/WritePage.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Editor from '../components/write/Editor';
-import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 
 // 글쓰기
 
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor />
-            <TagBox />
+            <EditorContainer />
+            <TagBoxContainer />
             <WriteActionButtons />
         </Responsive>
     );
