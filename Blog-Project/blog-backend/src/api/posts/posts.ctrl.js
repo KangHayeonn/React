14b67c578630945e4b3a0e788ded63b430ac3aef
KangHayeonn <rkdgkdus522@naver.com>
2022-01-31 14:04:56 +0900

    포스트 목록 페이지 : HTML 필터링하기
    * 백엔드 sanitize-html 을 이용해 HTML 태그를 제거하고 특정 HTML만
      허용하는 기능 추가
    
    - 작성자 : 강하연
    - 작성일 : 2022-01-31

--- Blog-Project/blog-backend/src/api/posts/posts.ctrl.js
+++ Blog-Project/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,8 +1,33 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHtml from "sanitize-html";
 
 const { ObjectId } = mongoose.Types;
+
+const sanitizeOption = {
+  allowedTags: [
+    "h1",
+    "h2",
+    "b",
+    "i",
+    "u",
+    "s",
+    "p",
+    "ul",
+    "ol",
+    "li",
+    "blockquote",
+    "a",
+    "img",
+  ],
+  allowedAttributes: {
+    a: ["href", "name", "target"],
+    img: ["src"],
+    li: ["class"],
+  },
+  allowedSchemes: ["data", "http"],
+};
 
 /*
 export const checkObjectId = (ctx, next) => {
@@ -70,7 +95,7 @@ export const write = async (ctx) => {
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
-    body,
+    body: sanitizeHtml(body, sanitizeOption),
     tags,
     user: ctx.state.user,
   });
@@ -80,6 +105,14 @@ export const write = async (ctx) => {
   } catch (e) {
     ctx.throw(500, e);
   }
+};
+
+// html을 없애고 내용이 너무 길면 200자로 제한하는 함수
+const removeHtmlAndShorten = body => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return (filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`); 
 };
 
 /* 포스트 목록 조회
@@ -116,8 +149,7 @@ export const list = async (ctx) => {
       .map((post) => post.toJSON())
       .map((post) => ({
         ...post,
-        body:
-          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+        body: removeHtmlAndShorten(post.body),
       }));
   } catch (e) {
     ctx.throw(500, e);
@@ -185,8 +217,14 @@ export const update = async (ctx) => {
     return;
   }
 
+  const nextData = { ...ctx.request.body }; //  객체를 복사하고
+  // body 값이 주어졌으면 HTML 필터링
+  if(nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
+  }
+
   try {
-    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    const post = await Post.findByIdAndUpdate(id, nextData, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환
       // false일 때는 업데이트 되기 전의 데이터를 반환
     }).exec();
